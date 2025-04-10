b7c5c813b924cc857f23a02ea81cf53485e6a79d
Jimmy Bates <jbates.productions@gmail.com>
2025-04-09 22:59:18 -0700

    SPI Update

--- inc/stm32f446xx_spi.h
+++ inc/stm32f446xx_spi.h
@@ -5,30 +5,17 @@
 
 #include "stm32f446xx.h"
 
-// SPI Master vs slave mode select:
+typedef enum { SPI_PERI_CLOCK_DISABLE = 0, SPI_PERI_CLOCK_ENABLE } SPIPeriClockEnable_t;
 typedef enum { SPI_DEVICE_MODE_SLAVE = 0, SPI_DEVICE_MODE_MASTER } SPIDeviceMode_t;
-
-// Bus configuration (simplex duplex etc)
 typedef enum {
   SPI_BUS_CONFIG_FULL_DUPLEX = 0,
   SPI_BUS_CONFIG_SIMPLEX_TX_ONLY,
   SPI_BUS_CONFIG_SIMPLEX_RX_ONLY,
   SPI_BUS_CONFIG_HALF_DUPLEX
 } SPIBusConfig_t;
-
-// Data frame format (i.e. 4-bit frames, 8-bit frames, etc)
-typedef enum {
-  SPI_DFF_8_BIT = 0,
-  SPI_DFF_16_BIT,
-} SPIDff_t;
-
-// Capture on rising edge while data stable, or falling edge
+typedef enum { SPI_DFF_8_BIT = 0, SPI_DFF_16_BIT } SPIDff_t;
 typedef enum { SPI_CAPTURE_MODE_RISING = 0, SPI_CAPTURE_MODE_FALLING } SPICaptureMode_t;
-
-// Software slave management (enable or disable)
 typedef enum { SPI_SSM_DISABLE = 0, SPI_SSM_ENABLE } SPISsm_t;
-
-// Baud divisor (Dictates the speed of the spi bus)
 typedef enum {
   SPI_BAUD_DIVISOR_2 = 0,
   SPI_BAUD_DIVISOR_4,
@@ -39,6 +26,20 @@ typedef enum {
   SPI_BAUD_DIVISOR_128,
   SPI_BAUD_DIVISOR_256
 } SPIBaudDivisor_t;
+typedef enum { SPI_INTERRUPT_DISABLE = 0, SPI_INTERRUPT_ENABLE } SPIInterruptEn_t;
+typedef enum { SPI_INTERRUPT_TYPE_TX = 0, SPI_INTERRUPT_TYPE_RX } SPIInterruptType_t;
+typedef enum { SPI_DMA_DISABLE = 0, SPI_DMA_ENABLE } SPIDMAEnable_t;
+
+typedef struct {
+  SPIInterruptEn_t en;
+  SPIInterruptType_t type;
+  uint16_t length;
+} SPIInterruptSetup_t;
+
+typedef struct {
+  SPIDMAEnable_t tx;
+  SPIDMAEnable_t rx;
+} SPIDMASetup_t;
 
 /**
  * @brief SPI configuration setup which is used to initiailize the SPI
@@ -57,6 +58,7 @@ typedef struct {
   SPIDff_t dff;
   SPICaptureMode_t capture_mode;
   SPISsm_t ssm;
+  SPIInterruptSetup_t interrupt_setup;
   SPIBaudDivisor_t baud_divisor;
 } SPIConfig_t;
 
@@ -71,15 +73,21 @@ typedef struct {
   SPIConfig_t cfg;
 } SPIHandle_t;
 
-int spi_peri_clock_control(const SPI_TypeDef *p_spi_addr, const uint8_t en);
+int spi_peri_clock_control(const SPI_TypeDef *p_spi_addr, const SPIPeriClockEnable_t en_state);
 
 int spi_init(const SPIHandle_t *p_spi_handle);
 
 int spi_deinit(const SPI_TypeDef *p_spi_addr);
 
-int spi_send_data(const SPI_TypeDef *p_spi_addr, uint8_t *p_tx_buffer, const uint32_t len);
+int spi_tx_byte(SPI_TypeDef *spi_port, const uint16_t tx_byte);
 
-int spi_receive_data(const SPI_TypeDef *p_spi_addr, uint8_t *p_rx_buffer, const uint32_t len);
+int spi_tx_word(SPI_TypeDef *spi_port, const void *tx_buffer, int len);
+
+uint16_t spi_rx_byte(SPI_TypeDef *spi_port);
+
+int spi_rx_word(SPI_TypeDef *spi_port, uint8_t *rx_buffer, int len);
+
+int spi_full_duplex_transfer(SPI_TypeDef *spi_port, void *tx_buffer, void *rx_buffer, int len);
 
 int spi_irq_handling(const SPI_TypeDef *p_spi_addr);
 
--- src/stm32f446xx_spi.c
+++ src/stm32f446xx_spi.c
@@ -12,7 +12,7 @@
 
 SPIConfig_t spi1_config = {0}, spi2_config = {0}, spi3_config = {0};
 
-int spi_peri_clock_control(const SPI_TypeDef *p_spi_addr, const uint8_t en_state) {
+int spi_peri_clock_control(const SPI_TypeDef *p_spi_addr, const SPIPeriClockEnable_t en_state) {
   if (p_spi_addr == NULL) return -1;  // Error: null pointer
 
   const SPI_TypeDef *spis_arr[] = SPIS;
@@ -79,6 +79,16 @@ int spi_init(const SPIHandle_t *p_spi_handle) {
     spi->CR1 |= (1 << SPI_CR1_CPHA_Pos);
   }
 
+  // Enable interrupts
+  if (cfg->interrupt_setup.en == SPI_INTERRUPT_ENABLE) {
+    if (cfg->interrupt_setup.type == SPI_INTERRUPT_TYPE_TX)
+      spi->CR2 |= (1 << SPI_CR2_TXEIE_Pos);
+    else if (cfg->interrupt_setup.type == SPI_INTERRUPT_TYPE_RX)
+      spi->CR2 |= (1 << SPI_CR2_RXNEIE_Pos);
+  }
+
+  // Enable DMA
+
   // Enable SPI
   spi->CR1 |= (1 << SPI_CR1_SPE_Pos);
 
@@ -86,6 +96,74 @@ int spi_init(const SPIHandle_t *p_spi_handle) {
 }
 
 int spi_deinit(const SPI_TypeDef *p_spi_addr) { return 0; }
+
+int spi_tx_byte(SPI_TypeDef *spi_port, const uint16_t tx_byte) {
+  if (spi_port == NULL) return -1;
+
+  // While the TX Buffer is not empty...
+  while (!(spi_port->SR & (1 << SPI_SR_TXE_Pos)));
+  spi_port->DR = tx_byte;
+
+  // Clear flag
+  spi_port->SR &= ~(1 << SPI_SR_TXE_Pos);
+
+  return 0;
+}
+
+int spi_tx_word(SPI_TypeDef *spi_port, const void *tx_buffer, int len) {
+  return spi_full_duplex_transfer(spi_port, (void *)tx_buffer, (void *)tx_buffer, len);
+}
+
+uint16_t spi_rx_byte(SPI_TypeDef *spi_port) {
+  if (spi_port == NULL) return 0;
+
+  // Wait until the rx is empty
+  while (!(spi_port->SR & (1 << SPI_SR_RXNE_Pos)));
+  uint16_t rx_word = spi_port->DR;
+
+  spi_port->SR &= ~(1 << SPI_SR_RXNE_Pos);
+  return rx_word;
+}
+
+int spi_rx_word(SPI_TypeDef *spi_port, uint8_t *rx_buffer, int len) {
+  return spi_full_duplex_transfer(spi_port, rx_buffer, rx_buffer, len);
+}
+
+int spi_full_duplex_transfer(SPI_TypeDef *spi_port, void *tx_buffer, void *rx_buffer, int len) {
+  if (spi_port == NULL) return -1;
+
+  // Get the amount of bytes per frame - Should be 1 bytes, or 2 bytes (dff=1)
+  uint8_t dff_bytes = ((spi_port->CR1 >> SPI_CR1_DFF_Pos) & 0b1) + 1;
+
+  while (len > 0) {
+    // Prepare the tx part of the transmission
+    uint16_t tx_word = 0;
+    if (dff_bytes == 1)
+      tx_word = *((uint8_t *)tx_buffer);
+    else {
+      tx_word = *((uint16_t *)tx_buffer);
+      if (len == 1) tx_word &= 0xFF00;
+    }
+
+    // Send byte
+    spi_tx_byte(spi_port, tx_word);
+
+    // Prepare the rx part of the transmission and receive bytes
+    uint16_t rx_byte = spi_rx_byte(spi_port);
+
+    if (dff_bytes == 1 || len == 1)
+      *((uint8_t *)rx_buffer) = rx_byte & 0xFF;
+    else
+      *((uint16_t *)rx_buffer) = rx_byte;
+
+    // Move buffer along to the next available frame
+    tx_buffer = (uint8_t *)tx_buffer + dff_bytes;
+    rx_buffer = (uint8_t *)rx_buffer + dff_bytes;
+    len -= dff_bytes;
+  }
+
+  return 0;
+}
 
 int spi_send_data(const SPI_TypeDef *p_spi_addr, uint8_t *p_tx_buffer, const uint32_t len) { return 0; }
 
