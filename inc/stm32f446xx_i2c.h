3c9c6bc531d22a3b44546d8d3d23ff164a2e5985
Jimmy Bates <jbates.productions@gmail.com>
2025-06-18 00:35:21 -0700

    Added structs for i2c

--- inc/stm32f446xx_i2c.h
+++ inc/stm32f446xx_i2c.h
@@ -6,4 +6,16 @@
 
 #include "stm32f446xx.h"
 
+typedef struct {
+  uint32_t scl_speed;
+  uint16_t fm_duty_cycle;
+  uint8_t device_address;
+  uint8_t ack_control;
+} I2CConfig_t;
+
+typedef struct {
+  I2C_TypeDef *addr;
+  I2CConfig_t cfg;
+} I2CHandle_t;
+
 #endif
