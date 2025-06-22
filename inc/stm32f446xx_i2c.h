715f9b328ad33ceac3bc088cc9039bae3037b09a
Jimmy Bates <jbates.productions@gmail.com>
2025-06-22 03:25:06 -0700

    Updates to i2c init method

--- inc/stm32f446xx_i2c.h
+++ inc/stm32f446xx_i2c.h
@@ -2,19 +2,38 @@
 #ifndef INC_STM34F446XX_I2C_H_
 #define INC_STM34F446XX_I2C_H_
 
+#include <stdatomic.h>
 #include <stdint.h>
 
 #include "stm32f446xx.h"
 
+typedef enum { I2C_MODE_SLAVE = 0, I2C_MODE_MASTER = 1 } I2CMode_t;
 typedef enum { I2C_SCL_SPEED_SM = 100000, I2C_SCL_SPEED_FM = 400000 } I2CSclSpeed_t;
 typedef enum { I2C_FM_DUTY_CYCLE_2 = 0, I2C_FM_DUTY_CYCLE_16_9 } I2CFMDutyCycle_t;
 typedef enum { I2C_DISABLE = 0, I2C_ENABLE } I2CEnable_t;
 
 typedef struct {
-  I2CSclSpeed_t scl_speed;
-  uint16_t fm_duty_cycle;
   uint8_t device_address;
+  I2CEnable_t clock_stretch;
+} I2CSlaveSetup_t;
+/**
+ * @brief I2C configuration structure
+ * @param peri_clock_speed The speed of the peripheral clock (APB1);
+ * @param scl_speed Whether I2C in standard, 100kHz, or fast, 400kHz, mode;
+ * @param fm_duty_cycle If fast mode, whether t_low = 2t_high, or 9t_low = 16t_high;
+ * @param device_address;
+ * @param ack_control;
+ */
+typedef struct {
+  uint32_t peri_clock_speed;
+  uint16_t fm_duty_cycle;
+  I2CMode_t mode;
+  I2CSlaveSetup_t slave_setup;
+  I2CEnable_t dma_enable;
+  I2CEnable_t interrupt_enable;
+  I2CSclSpeed_t scl_speed;
   I2CEnable_t ack_control;
+  I2CEnable_t enable_on_init;
 } I2CConfig_t;
 
 typedef struct {
@@ -24,6 +43,6 @@ typedef struct {
 
 int i2c_peri_clock_control(const I2C_TypeDef *i2c_reg, const I2CEnable_t en);
 int i2c_init(I2CHandle_t *i2c_handle);
-void i2c_deinit(I2C_TypeDef *i2c_reg);
+int i2c_deinit(I2C_TypeDef *i2c_reg);
 
 #endif
--- src/stm32f446xx_i2c.c
+++ src/stm32f446xx_i2c.c
@@ -10,7 +10,30 @@
 #define SIZEOF(arr) ((int)sizeof(arr) / sizeof(arr[0]))
 #define SIZEOFP(arr) ((int)sizeof(arr) / sizeof(uint32_t))  // Memory size of stm32f4
 
-int i2c_peri_clock_control(const I2C_TypeDef *i2c_reg, const I2CEnable_t en) { return 0; }
+int i2c_peri_clock_control(const I2C_TypeDef *i2c_reg, const I2CEnable_t en) {
+  if (i2c_reg == NULL) return -1;  // Error: null pointer
+
+  const I2C_TypeDef *i2cs_arr[] = I2CS;
+  int i = 0;
+
+  for (; i < SIZEOFP(i2cs_arr); i++) {
+    if (i2cs_arr[i] == i2c_reg) break;
+  }
+
+  if (i >= SIZEOFP(i2cs_arr)) return -1;
+
+  volatile uint32_t *i2c_regs[] = I2CS_RCC_REGS;
+  const unsigned int i2c_rcc_pos[] = I2CS_RCC_POS;
+
+  if (en == I2C_ENABLE)
+    *i2c_regs[i] |= (1 << i2c_rcc_pos[i]);
+  else if (en == I2C_DISABLE)
+    *i2c_regs[i] &= ~(1 << i2c_rcc_pos[i]);
+  else
+    return -2;
+
+  return 0;
+}
 
 int i2c_init(I2CHandle_t *i2c_handle) {
   if (i2c_handle == NULL) return -1;
@@ -18,7 +41,50 @@ int i2c_init(I2CHandle_t *i2c_handle) {
   I2C_TypeDef *addr = i2c_handle->addr;
   I2CConfig_t *cfg = &i2c_handle->cfg;
 
+  // Make sure freq can be set
+  uint8_t freq_mhz = cfg->peri_clock_speed / (int)1E6;
+  if (freq_mhz < 2 || freq_mhz > 50) return -2;
+
+  ///// CR1 register: /////
+  int32_t cr1_word = 0;
+
+  // Whether an acknowledge should be received after matched address or data
+  if (cfg->ack_control) cr1_word |= (1 << I2C_CR1_ACK_Pos);
+
+  // If in slave mode, whether to set in stretch mode (0 is stretch on stm32)
+  if (!cfg->slave_setup.clock_stretch) cr1_word |= (1 << I2C_CR1_NOSTRETCH_Pos);
+
+  addr->CR1 = cr1_word;
+
+  ///// CR2 register: /////
+  uint32_t cr2_word = 0;
+
+  // DMA Enable
+  if (cfg->dma_enable) cr2_word = (1 << I2C_CR2_DMAEN_Pos);
+
+  // Interrupt enable
+  if (cfg->interrupt_enable) {
+    cr2_word |= (1 << I2C_CR2_ITBUFEN_Pos);
+    cr2_word |= (1 << I2C_CR2_ITEVTEN_Pos);
+  }
+
+  cr2_word |= (freq_mhz << I2C_CR2_FREQ_Pos);
+
+  addr->CR2 = cr2_word;
+
+  // Slave address
+  addr->OAR1 = ((cfg->slave_setup.device_address & 0x7F) << I2C_OAR1_ADD1_Pos);
+
+  // NEED SPEED STUFF HERE
+
+  if (cfg->enable_on_init) addr->CR1 |= (1 << I2C_CR1_PE_Pos);
   return 0;
 }
 
-void i2c_deinit(I2C_TypeDef *i2c_reg) {}
+int i2c_deinit(I2C_TypeDef *i2c_reg) {
+  if (i2c_reg == NULL) return -1;
+
+  memset(i2c_reg, 0, sizeof(I2C_TypeDef));
+
+  return 0;
+}
