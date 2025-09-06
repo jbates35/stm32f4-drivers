69e5add8d7324dd612e1453b216b5f47d6ec2bb4
Jimmy Bates <jbates.productions@gmail.com>
2025-09-05 23:34:20 -0700

    Added main import... thought that was in there already?

--- src/stm32f446xx_tim.c
+++ src/stm32f446xx_tim.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+#include "stm32f446xx.h"
+
 #define TIMERS {TIM1, TIM2, TIM3, TIM4, TIM5, TIM6, TIM7, TIM8, TIM9, TIM10, TIM11, TIM12, TIM13, TIM14}
 #define TIMERS_RCC_REGS                                                                                     \
   {&RCC->APB2ENR, &RCC->APB1ENR, &RCC->APB1ENR, &RCC->APB1ENR, &RCC->APB1ENR, &RCC->APB1ENR, &RCC->APB1ENR, \
