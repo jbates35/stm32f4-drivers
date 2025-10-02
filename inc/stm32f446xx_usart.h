65ab0e495103ec015d9d28e501289d23f42b3789
Jimmy Bates <jbates.productions@gmail.com>
2025-10-02 00:43:45 -0700

    Some updates to usart

--- inc/stm32f446xx_usart.h
+++ inc/stm32f446xx_usart.h
@@ -23,15 +23,30 @@ typedef enum {
   USART_BAUD_RATE_460800 = 460800,
   USART_BAUD_RATE_921600 = 921600
 } USARTBaudRate_t;
+typedef enum { USART_STOP_BIT_ZERO = 0, USART_STOP_BIT_ONE, USART_STOP_BIT_TWO } USARTStopBitCount_t;
+typedef enum { asdf } USARTWordLength_t;
 
 typedef struct {
   uint32_t peri_clock_freq_hz;
   USARTMode_t mode;
-  uint8_t baud_rate;
-  uint8_t stop_bit_count;
+  USARTBaudRate_t baud_rate;
+  USARTStopBitCount_t stop_bit_count;
   uint8_t word_length;
   uint8_t parity_type;
   uint8_t hw_flow_control;
 } USARTConfig_t;
+typedef enum { USART_DISABLE = 0, USART_ENABLE } USARTEnable_t;
+
+typedef struct {
+  int asdf2;
+} USARTHandle_t;
+
+int usart_peri_clock_control(const USART_TypeDef *usart_reg, const USARTEnable_t en_state);
+int usart_init(const USARTHandle_t *usart_handle, const USARTEnable_t en_state);
+int usart_deinit(const USART_TypeDef *usart_reg);
+int usart_tx_byte_blocking(const USART_TypeDef *usart_reg, uint16_t tx_buff);
+uint16_t usart_rx_byte_blocking(const USART_TypeDef *usart_reg);
+int usart_tx_word_blocking(const USART_TypeDef *usart_reg, void *tx_buff, uint16_t len);
+int usart_rx_word_blocking(const USART_TypeDef *usart_reg, void *rx_buff, uint16_t len);
 
 #endif
